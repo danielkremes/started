dd022e4fd263df77450a13bd64bc073b72b943bd
danielkremes <danielkremes06@gmail.com>
2024-09-24 18:53:22 -0300

    add file: 'assignment_operators.py' inside folder 'types_of_python_operators'

---
+++ types_of_python_operators/assignment_operators.py
@@ -0,0 +1,23 @@
+balance = 500;
+print("Balance normal : " , balance)
+
+balance += 100;
+print("Balance update : ",  balance)
+
+balance -= 50;
+print("Balance after subtracting : ", balance)
+
+balance *= 2;
+print("Balance after doubling : ", balance)
+
+balance /= 5;
+print("Balance after dividing by 5 : ", balance)
+
+balance %= 3;
+print("Balance after performing modulo operation : ", balance)
+
+balance **= 2;
+print("Balance after raising to the power of 2 : ", balance)
+
+balance //= 7;
+print("Balance after integer division by 7 : ", balance)
