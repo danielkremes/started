c0f09466e6647bed778830c81443201eb921b19b
danielkremes <danielkremes06@gmail.com>
2024-09-24 20:06:57 -0300

    add file: 'association_operators.py' inside folder 'types_of_python_operators'

---
+++ types_of_python_operators/association_operators.py
@@ -0,0 +1,11 @@
+course = "Course Python"
+fruits = ["Apple", "Orange", "Pineapple", "Peach","Strawberry", "Lemon","Watermelon"]
+withdrawals = [1500,1000]
+
+print("Apple" in fruits)
+print("Orange" in fruits)
+print("Pineapple" in fruits)
+print("Avocado" not in fruits)
+
+print(300 in withdrawals)
+print(300 not in withdrawals)
