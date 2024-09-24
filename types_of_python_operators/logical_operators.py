8a3457d52be88f43ea4abd034d41f25b30c16a1e
danielkremes <danielkremes06@gmail.com>
2024-09-24 18:59:05 -0300

    add file: 'logical_operators.py' inside folder 'types_of_python_operators'

---
+++ types_of_python_operators/logical_operators.py
@@ -0,0 +1,6 @@
+balance = 1500
+withdraw = 200;
+
+print(balance >= withdraw and withdraw <= balance)
+print(not balance >= withdraw and not withdraw <= balance)
+print(balance >= withdraw or withdraw <= balance)
