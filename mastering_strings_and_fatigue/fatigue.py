248e05a53f018f11109ac56dbff5d4ecc493a085
danielkremes <danielkremes06@gmail.com>
2024-09-30 18:46:00 -0300

    add file 'fatigue..py' inside folder 'mastering_strings_and_fatigue'

---
+++ mastering_strings_and_fatigue/fatigue.py
@@ -0,0 +1,14 @@
+name = "Daniel Renato Kremes"
+nameFatigueOne = name[0:6]
+nameFatigueTwo = name[0:2]
+nameFatigueThree = name[0:2:2]
+nameFatigueFour = name[:3] 
+nameFatigueFive = name[2:]
+namePassword = name[::-1]
+
+print(nameFatigueOne)
+print(nameFatigueTwo)
+print(nameFatigueThree)
+print(nameFatigueFour)
+print(nameFatigueFive)
+print(namePassword)
