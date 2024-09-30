4628d1e5dc5b2e03e46ac62dc4e34011e7dbe5b8
danielkremes <danielkremes06@gmail.com>
2024-09-30 18:32:20 -0300

    add folder ' mastering_strings_and_fatigue' and file  'string_methods_01.py'  inside folder

---
+++ mastering_strings_and_fatigue/string_methods_01.py
@@ -0,0 +1,14 @@
+name = 'TeStiNG'
+
+print(name.lower())
+print(name.capitalize())
+print(name.upper())
+print(name.count('t'))
+print(name.replace('T', 't'))
+print(name.find('i'))
+print(name.startswith('Te'))
+print(name.rstrip())
+print(name.lstrip())
+print('-'.join(name))
+name_list = name.split('s')
+print(name_list)
