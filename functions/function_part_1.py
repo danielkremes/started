b523f19fff4b9873c1c2651b0fd1214463f09be9
danielkremes <danielkremes06@gmail.com>
2024-10-01 19:44:22 -0300

    add folder 'funtions' and inside of the folder file 'function_part_1.py'

---
+++ functions/function_part_1.py
@@ -0,0 +1,14 @@
+def show_name(name,age):
+    print(f"Hi {name}, age: {age}")
+
+def age(age):
+    return age
+
+age_value = age(24)
+show_name("Daniel",age_value)
+
+def store_cars(model, name_car, year, plate):
+    print(f"Car: {model}, Name: {name_car}, Year: {year}, Plate: {plate}")
+
+store_cars(model="SUV", name_car="TUCSON",year=2013, plate="XVG20");
+
